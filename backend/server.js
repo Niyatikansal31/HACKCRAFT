ad641ed3eedd543aa057570a93fd71c24293f729
Niyati Kansal <niyatikansal95@gmail.com>
2026-03-28 23:30:31 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = 'mongodb+srv://niyatikansal:August3108@cluster0.dpkkfjb.mongodb.net/HackCraft';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://niyatikansal:August3108@cluster0.dpkkfjb.mongodb.net/HackCraft';
 
 app.use(
   cors({
